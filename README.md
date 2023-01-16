a153db767abfa4c18f851689d81b11914bfe117c
xchdata.io <contact@xchdata.io>
2023-01-16 21:06:24 +0100

    Initial version: bech32m_{encode,decode}, blob_from_hex, chia_amount_int functions

---
+++ README.md
@@ -0,0 +1,58 @@
+# sqlite-chia
+
+An extension for SQLite, written in Rust, which provides Chia utility
+functions.
+
+## Installation
+
+Run `cargo build --release --features build_extension` to build a shared
+library loadable as SQLite extension.
+
+Optionally, you can manually strip the library to decrease binary size:
+`strip --strip-all target/release/libchia.so`.
+
+## Exposed extension functions
+
+- `bech32m_encode(text, blob) -> text`: Takes a prefix as first argument and
+  a blob as second and bech32m-encodes the blob with the given prefix into
+  a string.
+- `bech32m_decode(text) -> blob`: Decodes a bech32m-encoded string into a blob.
+- `blob_from_hex(string) -> blob`: Hex-decodes a string ('cafe') into a blob
+  (x'cafe').
+- `chia_amount_int(blob) -> integer`: Parse a Chia amount blob into an integer
+  (representing mojos).
+
+## Dependencies & References
+
+Binding to SQLite's [loadable extension interface][loadext] is handled by
+[rusqlite] extended with support for creating loadable extensions (see [pull
+request #910][pr910] or [corresponding branch][rusqlite-le]).
+
+[loadext]: https://www.sqlite.org/loadext.html
+[pr910]: https://github.com/rusqlite/rusqlite/pull/910
+[rusqlite-le]: https://github.com/Genomicsplc/rusqlite/tree/loadable-extensions
+[rusqlite]: https://github.com/rusqlite/rusqlite
+
+## License
+
+SPDX-License-Identifier: MIT
+
+Copyright (C) 2022-2023 xchdata.io
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ src/lib.rs
@@ -0,0 +1,147 @@
+use std::convert::TryInto;
+use std::io::Cursor;
+
+use rusqlite::functions::{Context, FunctionFlags};
+use rusqlite::types::{ToSqlOutput, Value};
+
+#[cfg(feature = "build_extension")]
+mod ext;
+
+fn ah(e: anyhow::Error) -> rusqlite::Error {
+    rusqlite::Error::UserFunctionError(format!("{:?}", e).into())
+}
+
+fn setup(db: &rusqlite::Connection) -> anyhow::Result<()> {
+    create_functions(&db)
+}
+
+fn create_functions(db: &rusqlite::Connection) -> anyhow::Result<()> {
+    let flags = FunctionFlags::SQLITE_UTF8 | FunctionFlags::SQLITE_INNOCUOUS |
+                FunctionFlags::SQLITE_DETERMINISTIC;
+    db.create_scalar_function("bech32m_encode",
+                                2,
+                                flags,
+                                |ctx| bech32m_encode_fn(ctx).map_err(ah))?;
+    db.create_scalar_function("bech32m_decode",
+                                1,
+                                flags,
+                                |ctx| bech32m_decode_fn(ctx).map_err(ah))?;
+    db.create_scalar_function("blob_from_hex",
+                                1,
+                                flags,
+                                |ctx| blob_from_hex_fn(ctx).map_err(ah))?;
+    db.create_scalar_function("chia_amount_int",
+                                1,
+                                flags,
+                                |ctx| chia_amount_int(ctx).map_err(ah))?;
+    Ok(())
+}
+
+fn bech32m_encode_fn<'a>(ctx: &Context) -> anyhow::Result<ToSqlOutput<'a>> {
+    use bech32::ToBase32;
+    let hrp = ctx.get::<String>(0)?;
+    let data = ctx.get::<Vec<u8>>(1)?;
+    let encoded = bech32::encode(&hrp, data.to_base32(), bech32::Variant::Bech32m)?;
+    Ok(ToSqlOutput::Owned(Value::Text(encoded)))
+}
+
+fn bech32m_decode_fn<'a>(ctx: &Context) -> anyhow::Result<ToSqlOutput<'a>> {
+    use bech32::FromBase32;
+    let encoded = ctx.get::<String>(0)?;
+    let (_hrp, data, _variant) = bech32::decode(&encoded)?;
+    Ok(ToSqlOutput::Owned(Value::Blob(Vec::<u8>::from_base32(&data)?)))
+}
+
+fn blob_from_hex_fn<'a>(ctx: &Context) -> anyhow::Result<ToSqlOutput<'a>> {
+    let hex = ctx.get::<String>(0)?;
+    let data =
+        (0..hex.len())
+            .step_by(2)
+            .map(|i| u8::from_str_radix(&hex[i..i+2], 16))
+            .collect::<Result<Vec<u8>, std::num::ParseIntError>>()?;
+    Ok(ToSqlOutput::Owned(Value::Blob(data)))
+}
+
+fn chia_amount_int<'a>(ctx: &Context) -> anyhow::Result<ToSqlOutput<'a>> {
+    let blob = ctx.get::<Vec<u8>>(0)?;
+    let bytes: [u8; 8] = blob.try_into().unwrap();
+    let mojos = i64::from_be_bytes(bytes);  // @@ i64 != u64
+    Ok(ToSqlOutput::Owned(Value::Integer(mojos)))
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use anyhow::Context;
+    pub use pretty_assertions::{assert_eq, assert_ne};
+
+    use rusqlite::Connection;
+
+    pub fn open_db() -> anyhow::Result<Connection> {
+        let db = Connection::open_in_memory().context("open in-memory db")?;
+        setup(&db).context("setup db")?;
+        Ok(db)
+    }
+
+    #[test]
+    fn sanity() -> anyhow::Result<()> {
+        open_db()?;
+        Ok(())
+    }
+
+    #[test]
+    fn bech32m_encode_works() -> anyhow::Result<()> {
+        let db = open_db()?;
+        assert_eq!("xch1jlgazv".to_string(),
+                   db.query_row("select bech32m_encode('xch', x'')",
+                                [],
+                                |r| r.get::<usize, String>(0))?);
+        assert_eq!("xch1etlqusgk05".to_string(),
+                   db.query_row("select bech32m_encode('xch', x'cafe')",
+                                [],
+                                |r| r.get::<usize, String>(0))?);
+        assert_eq!("xch17nmv5574vggcdxchqh8zjunt44ax05cwhcqz5e29pvf6mwc95e5s27yfa4".to_string(),
+                   db.query_row("select bech32m_encode('xch', x'f4f6ca53d56211869b1705ce29726bad7a67d30ebe002a65450b13adbb05a669')",
+                                [],
+                                |r| r.get::<usize, String>(0))?);
+        Ok(())
+    }
+
+    #[test]
+    fn bech32m_decode_works() -> anyhow::Result<()> {
+        let db = open_db()?;
+        assert_eq!("".to_string(),
+                   db.query_row("select hex(bech32m_decode('xch1jlgazv'))",
+                                [],
+                                |r| r.get::<usize, String>(0))?);
+        assert_eq!("CAFE".to_string(),
+                   db.query_row("select hex(bech32m_decode('xch1etlqusgk05'))",
+                                [],
+                                |r| r.get::<usize, String>(0))?);
+        assert_eq!("F4F6CA53D56211869B1705CE29726BAD7A67D30EBE002A65450B13ADBB05A669".to_string(),
+                   db.query_row("select hex(bech32m_decode('xch17nmv5574vggcdxchqh8zjunt44ax05cwhcqz5e29pvf6mwc95e5s27yfa4'))",
+                                [],
+                                |r| r.get::<usize, String>(0))?);
+        Ok(())
+    }
+
+    #[test]
+    fn blob_from_hex_works() -> anyhow::Result<()> {
+        let db = open_db()?;
+        assert_eq!("CAFE".to_string(),
+                   db.query_row("select hex(blob_from_hex('cafe'))",
+                                [],
+                                |r| r.get::<usize, String>(0))?);
+        Ok(())
+    }
+
+    #[test]
+    fn chia_amount_int_works() -> anyhow::Result<()> {
+        let db = open_db()?;
+        assert_eq!(5509699999997u64,
+                   db.query_row("select chia_amount_int(x'00000502D3B618FD')",
+                                [],
+                                |r| r.get::<usize, u64>(0))?);
+        Ok(())
+    }
+}
